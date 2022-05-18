b1f6cd0ee69291ee6256bd7a1dcf6d882aceed1e
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2022-05-18 12:49:57 -0500

    added a loop and option to continue or exit

--- RandNumGuesser.java
+++ RandNumGuesser.java
@@ -5,21 +5,38 @@ public class RandNumGuesser
 {
     public static void main(String args[])
     {
-        System.out.print("Enter a number (1-10): ");
-        Scanner scan = new Scanner(System.in);
-
-        int user = scan.nextInt();
-        int computer = (int)(Math.random() * 10) + 1; 
-
-        if (user == computer)
+        boolean TF = true;
+        while (TF == true)
         {
-            System.out.println("Computer guesses " + computer + ".");
-            System.out.println("You lose!");
-        }
-        else
-        {
-            System.out.println("Computer guesses " + computer + ".");
-            System.out.println("You win!");
+            System.out.print("Enter a number (1-10): ");
+            Scanner scan = new Scanner(System.in);
+    
+            int user = scan.nextInt();
+            int computer = (int)(Math.random() * 10) + 1; 
+    
+            if (user == computer)
+            {
+                System.out.println("Computer guesses " + computer + ".");
+                System.out.println("You lose!");
+            }
+            else
+            {
+                System.out.println("Computer guesses " + computer + ".");
+                System.out.println("You win!");
+            }
+
+            System.out.print("Enter 'e' to exit or 'p' to play again: ");
+            String decision = scan.next();
+
+            if (decision.equals("e"))
+            {
+                TF = false;
+            }
+            else if (decision.equals("p"))
+            {
+                TF = true;
+            }
+
         }
     }
 }
