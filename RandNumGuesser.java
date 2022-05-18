629c1bd485cf2b64208a18180983c1c8da8b15f8
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2022-05-17 21:07:55 -0500

    created a main method and added the user and computer variables

--- RandNumGuesser.java
+++ RandNumGuesser.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class RandNumGuesser 
 {
-    
+    public static void main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+
+        int userNum = scan.nextInt();
+        int computer = (int)Math.random() * 10;
+    }
 }
